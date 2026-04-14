f9f9f1b2fb8b7918314f2cc3e4aa5fb42daf8afb
charansagala <scharan2455@gmail.com>
2026-04-14 08:30:20 +0530

    Initial commit

---
+++ basic/Prime.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Prime{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number to check if it is prime:");
+        int n=sc.nextInt();
+        boolean isPrime = true;
+        if(n<=1){
+            isPrime = false;
+        }else{
+            for(int i=2;i<=Math.sqrt(n);i++){
+                if(n%i==0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime)
+            System.out.print(n+" is a prime number.");
+            else
+            System.out.print(n+" is not a prime number.");  
+        sc.close();
+        }
+        }
+    }
