99dc9de4000f80014f01a651c4284bcd6e08dc11
charansagala <scharan2455@gmail.com>
2026-06-20 22:25:19 +0530

    Add Divisors solution

---
+++ basic/Divisors.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Divisors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int n = sc.nextInt();
+        System.out.println();
+        ArrayList<Integer> divisors = new ArrayList<>();
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                divisors.add(i);
+                if (i != n / i) {
+                    divisors.add(n / i);
+                }
+            }
+        }
+        Collections.sort(divisors);
+        for (int d : divisors) {
+            System.out.print(d + " ");
+        }
+        sc.close();
+    }
+}
