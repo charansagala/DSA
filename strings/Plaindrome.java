c4e789143744deb66633995fcf788f8f1f8d0650
charansagala <scharan2455@gmail.com>
2026-07-02 12:43:05 +0530

    Add palindrome string example

---
+++ strings/Plaindrome.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Palindrome{
+    public void static main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string:");
+        String s = sc.next();
+    int str = 0 ;
+    int end = s.length()-1;
+    bool isAlphaNum(char ch){
+
+    }
+    while(str < end){
+    if(!isAlphaNum(s[str])){
+        str++;continue;
+    if(!isAlphaNum())
+    }
+    }
+    }
+}
