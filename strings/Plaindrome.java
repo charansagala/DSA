8121b4b422af1e1fc570ebc00834b6698b25efd8
charansagala <scharan2455@gmail.com>
2026-07-04 00:13:07 +0530

    Update Plaindrome.java

--- strings/Plaindrome.java
+++ strings/Plaindrome.java
@@ -1,19 +1,40 @@
-import java.util.*;
-class Palindrome{
-    public void static main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the string:");
-        String s = sc.next();
-    int str = 0 ;
-    int end = s.length()-1;
-    bool isAlphaNum(char ch){
+=import java.util.*;
 
+class Palindrome {
+
+    public static boolean isAlphaNum(char ch) {
+        return Character.isLetterOrDigit(ch);
     }
-    while(str < end){
-    if(!isAlphaNum(s[str])){
-        str++;continue;
-    if(!isAlphaNum())
-    }
-    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String s = sc.nextLine();
+        int start = 0;
+        int end = s.length() - 1;
+        boolean isPalindrome = true;
+        while (start < end) {
+            if (!isAlphaNum(s.charAt(start))) {
+                start++;
+                continue;
+            }
+            if (!isAlphaNum(s.charAt(end))) {
+                end--;
+                continue;
+            }
+            char left = Character.toLowerCase(s.charAt(start));
+            char right = Character.toLowerCase(s.charAt(end));
+            if (left != right) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+        sc.close();
     }
 }
